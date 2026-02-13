7b88ac0cd32bc291f372b3144372f07f8cc5f069
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-02-13 18:40:15 +0500

    fea :custom asyncHandler defined

--- backend-authentications/src/utils/asyncHandler.js
+++ backend-authentications/src/utils/asyncHandler.js
@@ -1,51 +1,9 @@
 
 
-
-
-
-
-
-
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await fn(req, res, next)
-//     } catch (error) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-/**
- * @desc Wraps async route handlers and forwards errors to Express error middleware
- * @param {Function} fn - async route handler
- * @returns {Function} wrapped route
- *
- * @example
- * router.get("/users/:id", asyncHandler(async (req, res, next) => {
- *    const user = await User.findById(req.params.id);
- *    res.json(user);
- * }));
- */
-const asyncHandler = (fn) => {
-  return async (req, res, next) => {
+const asyncHandler = (fn) => async (req, res , next )=>{
     try {
       await fn(req, res, next);
-    } catch (err) {
-      next(err);
+    } catch (error) {
+      next(error)
     }
-  };
-};
-
-export { asyncHandler };
+  }
