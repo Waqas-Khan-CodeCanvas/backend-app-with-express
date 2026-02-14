cdb2f6ef1d268d71bef1b0083e907da208772f31
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-02-14 19:34:28 +0500

    configure user model

--- backend-authentications/src/models/user.model.js
+++ backend-authentications/src/models/user.model.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username:{type:String , required:true , trim:true},
     useremail:{type:String , required:true , trim:true , unique:true},
-    password:{type:String , required:true , trim:true},
-    refreshToken:{type:String , default:""},
+    password:{type:String , required:true , trim:true , select:false},
+    refreshToken:{type:String , default:"" , select:false},
     isActive:{type:Boolean , default:true},
     role:{type:String , enum:["user" , "admin"] , default:"user"},
     userProfileImgUrl:{type:String , default:""},
