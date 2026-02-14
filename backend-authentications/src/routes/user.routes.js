9bb58e40754fcf6d7bcd9252781dd8b49aa10991
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-02-14 19:33:29 +0500

    configure auth routes

---
+++ backend-authentications/src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import express from "express"
+import {} from "../controllers/user.controller.js"
+
+const router = express.Router();
+
+// router.post("/profile" , );
+
+
+
+export default router;
